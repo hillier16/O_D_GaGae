caaa5b897a3aace846b8103e41979b14adc4e67b
Subeen <hillier@khu.ac.kr>
2019-12-29 23:22:00 +0900

    add restframework

--- O_D_GaGae/settings.py
+++ O_D_GaGae/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'delos',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- O_D_GaGae/urls.py
+++ O_D_GaGae/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('delos.urls')),
     path('', include('delos.urls')),
 ]
