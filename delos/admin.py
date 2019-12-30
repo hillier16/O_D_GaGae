bd59e822db9372c152e8878f31133b4b99512f3e
kimhanju <kimhanju7@naver.com>
2019-12-30 17:12:49 +0900

    UPDATE models for ORM

--- delos/admin.py
+++ delos/admin.py
@@ -3,8 +3,12 @@ from .models import *
 # Register your models here.
 
 admin.site.register(User)
-admin.site.register(Member)
-admin.site.register(Groupschedule)
-admin.site.register(Timeschedule)
 admin.site.register(Group)
+admin.site.register(GroupMember)
+admin.site.register(Groupschedule)
+admin.site.register(GroupNotice)
+admin.site.register(GroupBoard)
+admin.site.register(Timeschedule)
 admin.site.register(Survey)
+admin.site.register(SurveyQuestion)
+admin.site.register(SurveyAnswer)
